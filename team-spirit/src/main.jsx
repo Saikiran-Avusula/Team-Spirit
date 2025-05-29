936b9130212acbb051d02d0a7ba02b01e1788de1
Saikiran-Avusula <saikiranavusula89@gmail.com>
2025-05-29 17:26:39 +0530

    Add routing and context provider to main application setup

--- team-spirit/src/main.jsx
+++ team-spirit/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // <- This is essential!
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+  import { CartProvider } from './components/context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
